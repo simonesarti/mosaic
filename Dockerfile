aafef3674c490969fc064d28fc95cd85278addcb
Simone Sarti <simonesarti98@gmail.com>
2023-07-21 12:41:27 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,16 @@ RUN apt-get update
 RUN apt-get install -y gdal-bin
 RUN apt-get install -y libgdal-dev
 RUN export CPLUS_INCLUDE_PATH=/usr/include/gdal && export C_INCLUDE_PATH=/usr/include/gdal && pip install GDAL
+
+RUN mkdir /.config
+RUN chmod 777 /.config
+
+RUN mkdir /.local
+RUN chmod 777 /.local
+
+RUN mkdir /.cache
+RUN chmod 777 /.cache
+
+COPY . /opt/ml/code/
+RUN cd /opt/ml/code/  && pip install -e .
+WORKDIR /opt/ml/code/
--- mosaic.py
+++ mosaic.py
@@ -1,6 +1,7 @@
 
 from argparse import ArgumentParser
 import datetime
+import shutil
 
 import mosaic.sentinel1
 import mosaic.sentinel2
@@ -8,26 +9,26 @@ import mosaic.esalulc
 import mosaic.dwlulc
 import mosaic.copernicusdem
 
-def get_parser(temporal):
-    #parser.add_argument("--bbox", type=str, default="46.00/-16.15/46.02/-16.01", help="bounding box: minlong/minlat/maxlong/maxlat")
+def parse_arguments():
 
     parser = ArgumentParser()
+    parser.add_argument("--image", type=str, choices=["sentinel1","sentinel2", "esalulc", "dwlulc", "copernicusdem"], default="copernicusdem", help="the image type to extract")
     parser.add_argument("--minlong", type=float, default=46.00, help="minimum value for longitude used to create the bounding box")
     parser.add_argument("--minlat", type=float, default=-16.15, help="minimum value for latitude used to create the bounding box")
     parser.add_argument("--maxlong", type=float, default=46.05, help="maximum value for longitude used to create the bounding box")
     parser.add_argument("--maxlat", type=float, default=-16.01, help="maximum value for latitude used to create the bounding box")
     parser.add_argument("--start", type=str, default="2020/10/5", help="start date, in format year/month/day")
     parser.add_argument("--end", type=str, default="2021/12/7", help="end date, in format year/month/day")
-    parser.add_argument("--split_shape", type=tuple, default=(10,10), help="bounding box splits in (row,columns)")
+    parser.add_argument("--split_rows", type=int, default=10, help="bounding box splits in n rows")
+    parser.add_argument("--split_columns", type=int, default=10, help="bounding box splits in n columns")
     parser.add_argument("--max_retry", type=int, default=10, help="maximimun number of requests for the same images")
     parser.add_argument("--output", type=str, default="./mosaic.tiff", help="output path")
-
-    if(temporal == True):
-        parser.add_argument("--n", type=int, default=3, help="number of periods to use")
+    parser.add_argument("--n", type=int, default=3, help="number of periods to use, when time is relevant")
     
     args = parser.parse_args()
     
     args.bbox = (args.minlong, args.minlat, args.maxlong, args.maxlat)
+    args.split_shape = (args.split_rows, args.split_columns)
 
     args.start = args.start.split("/")
     args.start = datetime.datetime(int(args.start[0]), int(args.start[1]), int(args.start[2]))
@@ -39,22 +40,31 @@ def get_parser(temporal):
     del args.minlat
     del args.maxlong
     del args.maxlat
+    del args.split_rows
+    del args.split_columns
     
     args = vars(args)
     
     return(args)
 
-def sentinel1():
-    mosaic.sentinel1.mosaic(**get_parser(temporal=True))
 
-def sentinel2():
-    mosaic.sentinel2.mosaic(**get_parser(temporal=True))
+if __name__ == "__main__":
 
-def dwlulc():
-    mosaic.dwlulc.mosaic(**get_parser(temporal=True))
+    args = parse_arguments()
 
-def esalulc():
-    mosaic.esalulc.mosaic(**get_parser(temporal=False))
+    if args.image in ["esalulc", "copernicusdem"]:
+        args.pop("n")    # not temporal
 
-def copernicusdem():
-    mosaic.copernicusdem.mosaic(**get_parser(temporal=False))
+    if args.image == "sentinel1":
+        from mosaic.sentinel1 import mosaic
+    if args.image == "sentinel2":
+        from mosaic.sentinel2 import mosaic
+    if args.image == "dwlulc":
+        from mosaic.dwlulc import mosaic
+    if args.image == "esalulc":
+        from mosaic.esalulc import mosaic
+    if args.image == "copernicusdem":
+        from mosaic.copernicusdem import mosaic
+
+    mosaic(**args)
+    shutil.rmtree("./test_dir")
